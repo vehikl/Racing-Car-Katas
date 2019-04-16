5946b1ceb4f4e165a5401eaca6a7ceeb6b9831d9
Vehikl Team <go@vehikl.com>
2019-04-16 11:33:37 -0400

    Get unicode-file-to-html system under tests with jest

---
+++ Javascript/__tests__/unicode-file-to-html-text-converter-tests/unicode-file-to-html-text-converter-test.js
@@ -0,0 +1,15 @@
+const UnicodeFileToHtmlTextConverter = require('../../unicode-file-to-html-text-converter/unicode-file-to-html-text-converter');
+
+describe('Unicode To Html Converter', function () {
+
+	describe('UnicodeFileToHtmlTextConverter', function () {
+
+		it('Do something', function () {
+
+			var target = new UnicodeFileToHtmlTextConverter();
+
+		});
+
+	});
+
+});
